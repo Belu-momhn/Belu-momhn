7bb6f8afcb3eb062e40b063bb7cbc15927fb7318
@Termux <133125035+Belu-momhn@users.noreply.github.com>
2023-05-21 21:30:28 +0300

    Create JavaScript.java
    <!DOCTYPE html>
    <html>
    
    <head>
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1">
      <title>fix <textarea name="" id="" cols="0" rows="10"></textarea></title>
    </head>
    
    <body>
    
    </body>
    
    </html>
    class="78"
    <div>78</div>
    <div><!doctype html></html></div>
    <img><head>you</head>

---
+++ JavaScript.java
@@ -0,0 +1,30 @@
+
+
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+  <meta charset="UTF-8">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>fix <textarea name="" id="" cols="0" rows="10"></textarea></title>
+
+</head>
+
+<body>
+
+</body>
+
+</html>
+
+class="78"
+
+<div>78</div>
+
+<div><!doctype html></html></div>
+
+<img><head>you</head>
